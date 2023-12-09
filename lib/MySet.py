47652ae0eda2e3a81d08918ff7956565e504d841
jmbeba <john.mbeba@student.moringaschool.com>
2023-12-09 12:42:32 +0300

    lab completed

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,33 @@
 class MySet:
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+            
+    def __str__(self):
+        keys = {x for x in self.dictionary.keys()}
+        keys_str = ','.join(str(x) for x in keys)
+        return f"MySet: {{{keys_str}}}"
+            
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self, value):
+        self.dictionary.pop(value,None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary = {}
+        return self
+    
+    
 
-    pass
+    
+    
